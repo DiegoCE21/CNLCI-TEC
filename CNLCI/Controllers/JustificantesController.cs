767e58b9fbaf435b9e904564ced2186cc5907b9a
JESSAMYN SELENE CORONADO CERVANTES <L19660185@matehuala.tecnm.mx>
2023-10-23 12:00:21 -0600

    23/10

--- CNLCI/Controllers/JustificantesController.cs
+++ CNLCI/Controllers/JustificantesController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using CNLCI.Models;
+using CNLCI.Models.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CNLCI.Controllers
 {
@@ -7,9 +10,75 @@ namespace CNLCI.Controllers
     {
         // GET: Justificantes
         public ActionResult Justificante()
-        { return View(); }
+        {
+            using (DB db = new DB())
+            {
+                string matricula = "";
+                var datos = db.Alumno.Where(a => a.Matricula == matricula).ToList();
+
+                return View(datos);
+            }
+        }
 
         public ActionResult PaseDeSalida()
-        { return View(); }
+
+        {
+            string matricula = "";
+            using (DB db = new DB())
+            {
+
+                var datos = (from Alumno in db.Alumno
+                             join Tutor in db.Tutor on Alumno.Matricula equals Tutor.Matricula
+                             where matricula == Alumno.Matricula
+                             select new MJ
+                             {
+                                 Matricula = Alumno.Matricula,
+                                 Nombre = Alumno.Nombre,
+                                 Primer_apellido = Alumno.Primer_apellido,
+                                 Segundo_apellido = Alumno.Segundo_apellido,
+                                 Tutor_Nombre = Tutor.Tutor_Nombre
+
+                             }).ToList();
+                return View("PaseDeSalida", datos);
+
+
+            }
+        }
+
+
+        public ActionResult Buscar(string matricula)
+        {
+
+            using (DB db = new DB())
+            {
+                var datos = db.Alumno.Where(a => a.Matricula == matricula).ToList();
+                return View("Justificante", datos);
+            }
+
+
+        }
+
+        public ActionResult BuscarP(string matricula)
+        {
+            using (DB db = new DB())
+            {
+                var datos = (from Alumno in db.Alumno
+                             join Tutor in db.Tutor on Alumno.Matricula equals Tutor.Matricula
+                             where matricula == Alumno.Matricula
+                             select new MJ
+                             {
+                                 Matricula = Alumno.Matricula,
+                                 Nombre = Alumno.Nombre,
+                                 Primer_apellido = Alumno.Primer_apellido,
+                                 Segundo_apellido = Alumno.Segundo_apellido,
+                                 Tutor_Nombre = Tutor.Tutor_Nombre
+
+                             }).ToList();
+                return View("PaseDeSalida", datos);
+
+
+            }
+
+        }
     }
 }
